b9d7471f220e66693f440546cfa095b6c89a8a26
flowmellow <michael.floresmmt@gmail.com>
2024-05-08 16:17:41 -0400

    Add project files.

---
+++ Assignment1.2Q1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Write a C# Sharp program to accept two integers and check whether they are equal or not.
+
+namespace Assignment1._2Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input 2 integers for testing data");
+            Console.WriteLine("Input 1st number: ");
+            int testOneNumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input 2nd number: ");            
+            int testOneNumber2 = Convert.ToInt32(Console.ReadLine());
+
+            if (testOneNumber1 == testOneNumber2)
+            {
+                Console.WriteLine(testOneNumber1 + " and " + testOneNumber2 + " are equal.");
+            }
+            else 
+            {
+                Console.WriteLine(testOneNumber1 + " and " + testOneNumber2 + " are not equal.");
+            }
+            Console.WriteLine(); // Added for spacing
+            Console.WriteLine("Thank you for using the application!");
+        }
+    }
+}
